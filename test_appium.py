63e320312af6e0940fb96611f93b60d1c2d63a32
lys861205 <lys86_1205@126.com>
2019-10-23 16:08:13 +0800

    add appium test demo

---
+++ test_appium.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+# coding=utf-8
+
+from appium import webdriver
+
+desired_capabilities = {}
+desired_capabilities["platformVersion"] =  "11.0"
+desired_capabilities["deviceName"] =  "iPhone 8"
+desired_capabilities["automationName"] =  "XCUITest"
+desired_capabilities["udid"] =  "34855823045694534883"
+desired_capabilities["bundleId"] =  "com.wemomo.momoappdemo1"
+desired_capabilities["platformName"] =  "iOS"
+
+def start_living():
+  try:
+    print(desired_capabilities)
+    driver = webdriver.Remote("http://10.129.148.209:4723/wd/hub", desired_capabilities)
+    #driver = webdriver.Remote("http://10.129.148.209:4723/wd/hub")
+    #driver.start_session(desired_capabilities)
+    print(driver.session)
+    driver.quit()
+  except Exception as err:
+    print(err)
+
+if __name__ == "__main__":
+  try:
+    print("testing appium...")
+    start_living()
+  except KeyboardInterrupt:
+    print("program is quit")
+  
+  
+
