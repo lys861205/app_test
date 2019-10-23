7944cbb66594717a755a4f89a2ae350595b75b74
MOMO <lys86_1205@126.com>
2019-10-23 08:22:32 +0800

    update file

--- momo.py
+++ momo.py
@@ -7,39 +7,36 @@ import unittest
 class MyTest(unittest.TestCase):
 
   def setUp(self):
-    try:
-      self.c = wda.Client("http://localhost:8100")
-      self.s = self.c.session("com.wemomo.momoappdemo1")
-    except Exception as err:
-      print(err)
+    self.c = wda.Client("http://localhost:8100")
+    self.s = self.c.session("com.wemomo.momoappdemo1")
 
   def test_living(self):
-    try:
-      #点击直播按钮
-      self.s(name="item_live", className="Image").tap()
+    #点击直播按钮
+    self.s(name="item_live", className="Image").tap()
 
-      #点击开播
-      btns = self.s(className="Button").find_elements()
-      btns[1].tap()
 
-      #点击"开始直播"
-      elems = self.s(className="StaticText").find_elements()
-      for e in elems:
-        if e.text == u"开始直播":
-          e.tap()
-          break
+    #点击开播
+    btns = self.s(className="Button").find_elements()
+    btns[1].tap()
 
-      #点击二级页“开始直播”
-      elems = self.s(name="", className="Button").tap()
-    except AttributeError as err:
-      print(err)
+    #点击"开始直播"
+    self.s(text=u"开始直播", className="StaticText").tap()
+
+    #点击二级页“开始直播”
+    self.s(name=u"开始直播", className="Button").tap()
+
+    self.s(name="D",className="Button").tap()
+    time.sleep(10)
+
+
   
   def tearDown(self):
-    try:
-      self.s.close()
-    except AttributeError as err:
-      print(err)
-    
+    self.s.close()
+
+
+
+
+
 if __name__ == "__main__":
   unittest.main()
 
